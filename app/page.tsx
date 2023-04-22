8ae2aef2a076bf8a7355c1780b39a99154119b34
ackidd <60202870+ackidd@users.noreply.github.com>
2023-04-22 16:36:42 -0400

    Working on the sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ function HomePage() {
     >
       <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
 
-      <div className='flex space-x-2 text-center'>
+      <div className="flex space-x-2 text-center">
         <div>
           <div className="flex flex-col items-center justify-center mb-5">
             <SunIcon className="h-8 w-8" />
@@ -20,11 +20,15 @@ function HomePage() {
           </div>
 
           <div className="space-y-2">
-            <p className="infoText">"Explain something to me."</p>
             <p className="infoText">
-              "What is the difference between a dog and a cat?
+              "Explain quantum computing in simple terms"
             </p>
-            <p className="infoText">"What is the color of the sun?"</p>
+            <p className="infoText">
+              "Got any creative ideas for a 10 year old's birthday?
+            </p>
+            <p className="infoText">
+              "How do I make an HTTP request in Javascript?"
+            </p>
           </div>
         </div>
 
@@ -35,11 +39,15 @@ function HomePage() {
           </div>
 
           <div className="space-y-2">
-            <p className="infoText">"Explain something to me"</p>
             <p className="infoText">
-              "What is the difference between a dog and a cat?
+              "Remembers what user said earlier in the conversation"
             </p>
-            <p className="infoText">"What is the color of the sun?"</p>
+            <p className="infoText">
+              "Allows user to provide follow-up corrections
+            </p>
+            <p className="infoText">
+              "Trained to decline inappropriate requests"
+            </p>
           </div>
         </div>
 
@@ -50,11 +58,15 @@ function HomePage() {
           </div>
 
           <div className="space-y-2">
-            <p className="infoText">"Explain something to me"</p>
             <p className="infoText">
-              "What is the difference between a dog and a cat?
+              "May occasionally generate incorrect information"
             </p>
-            <p className="infoText">"What is the color of the sun?"</p>
+            <p className="infoText">
+              "May occasionally produce harmful instructions or biased content
+            </p>
+            <p className="infoText">
+              "Limited knowledge of world and events after 2021"
+            </p>
           </div>
         </div>
       </div>
