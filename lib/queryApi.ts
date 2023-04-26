1f3d3055b19e9b9e427f6d506737e6cc2f5ff773
ackidd <60202870+ackidd@users.noreply.github.com>
2023-04-26 01:50:56 -0400

    Trying to debug ChatGPT error 429

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -1,22 +1,24 @@
-import openai from "./chatgpt";
+import openai from './chatgpt';
 
 const query = async (prompt: string, chatId: string, model: string) => {
-  const res = await openai.createCompletion({
-    model,
-    prompt,
-    temperature: 0.9,
-    top_p: 1,
-    max_tokens: 1000,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0
-  }).then(res => res.data.choices[0].text)
+  const res = await openai
+    .createCompletion({
+      model,
+      prompt,
+      temperature: 0.9,
+      top_p: 1,
+      max_tokens: 1000,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    })
+    .then((res) => res.data.choices[0].text)
+    .catch(
+      (err) =>
+        `ChatGPT was unable to find an answer for that! (Error: ${err.message})`
+    );
 
-
-  .catch((err) => `ChatGPT was unable to find an answer for that! (Error: ${err.message})`);
-
-  return res
-}
+  return res;
+};
 
 export default query;
-
-
+ 
